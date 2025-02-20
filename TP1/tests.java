0573c8fa167db140d3141146aef81468f9cbe6e0
Ricardo Santinho Horta <Ricardosanhorta@gmail.com>
2025-02-20 16:31:24 +0000

    Commit 2

--- TP1/tests.java
+++
@@ -1,9 +0,0 @@
-public class tests {
-    public static void main(String[] args) {
-        
-        //Exercício 2
-        System.out.println('b');
-        System.out.println('b'+ 'c');
-        System.out.println((char) ('a'+ 4));
-    }
-}
